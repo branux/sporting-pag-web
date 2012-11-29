a737a0e3d1c066de0f61bc2dbd4734c356d3e404
nicofetter@gmail.com <nicofetter@gmail.com@152097ae-678e-6705-a9aa-b76f55aa2b9e>
2012-11-29 00:18:47 +0000

    Agregue el proyecto para visual 2008

---
+++ trunk/Project/SportingWeb/SportingWeb/App_Code/Dominio/SportingException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportingWeb
+{
+    public class SportingException : Exception
+    {
+        private String message;
+
+        public SportingException(String message)
+	    {
+            this.Message = message;
+	    }
+        
+    
+        public String Message
+        {
+            get { return this.message; }
+            set { this.message = value; }
+        }
+    }
+}
