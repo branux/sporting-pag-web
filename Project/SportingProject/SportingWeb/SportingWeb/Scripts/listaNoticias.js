b5d630a48afd63b62bed89459f43641431dced1c
nicofetter <nicofetter@gmail.com>
2012-12-27 17:26:55 +0000

    casi listo listado de noticias, me falta agregar paginado. Nuevo BackUpsBaseDatos/sporting 20121227 1426.sql

---
+++ Project/SportingProject/SportingWeb/SportingWeb/Scripts/listaNoticias.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function() {
+        getNoticias();
+});
+
+function getNoticias() {
+        $.ajax({
+              type: "POST",
+              url: "WebServiceNoticias.asmx/GetAllNoticias",
+              data: "{}",
+              contentType: "application/json; charset=utf-8",
+              dataType: "json",
+              success: function(response) {
+                    var noticias = response.d;
+                    $('#grillaNoticias').empty();
+                    $.each(noticias, function(index, not) {
+                        $('#grillaNoticias').append('<div class="itemNoticia">' +
+                                                        '<img src="'+getPortada(not.Imagenes)+'"/>' +
+                                                        '<h3>' + not.Titulo + '</h3>' +
+                                                        '<p>' + not.Descripcion + '</p>' +
+                                                    '</div>');
+                    });
+              },
+              failure: function(msg) {
+                $('#output').text(msg);
+              }
+        });
+  };
+  
+  function getPortada(imagenes) {
+        var portada = null;
+        if (imagenes != null)
+        {
+            for (var i=0;i<imagenes.length;i++)
+            { 
+                if (imagenes[i].Portada)
+                {
+                    portada = imagenes[i];
+                }
+            }
+        }
+        return portada.PathSmall;
+  };
+
